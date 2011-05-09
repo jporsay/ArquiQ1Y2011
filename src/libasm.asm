6a75c0e60af8fb1e822cb8e9798779e39e4be739
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-08 23:28:34 -0300

    changed default indentations

--- src/libasm.asm
+++ src/libasm.asm
@@ -11,92 +11,93 @@ SECTION .text
 
 
 _Cli:
-	cli			; limpia flag de interrupciones
+	cli	; limpia flag de interrupciones
 	ret
 
 _Sti:
 
-	sti			; habilita interrupciones por flag
+	sti	; habilita interrupciones por flag
 	ret
 
 _outb:
 	push	ebp
-	mov	ebp, esp
-	mov	edx, [ss:ebp+8] ;Grab data
-	mov	eax, [ss:ebp+12] ;Grab port
-	out	dx, ax
-	pop ebp
+	mov		ebp, esp
+	mov		edx, [ss:ebp+8] ;Grab data
+	mov		eax, [ss:ebp+12] ;Grab port
+	out		dx, ax
+	pop		ebp
 	ret
 
 
 _mascaraPIC1:			; Escribe mascara del PIC 1
-	push    ebp
-        mov     ebp, esp
-        mov     ax, [ss:ebp+8]  ; ax = mascara de 16 bits
-        out	21h,al
-        pop     ebp
-        retn
+	push	ebp
+	mov		ebp, esp
+	mov		ax, [ss:ebp+8]  ; ax = mascara de 16 bits
+	out		21h,al
+	pop		ebp
+	retn
 
 _mascaraPIC2:			; Escribe mascara del PIC 2
-	push    ebp
-        mov     ebp, esp
-        mov     ax, [ss:ebp+8]  ; ax = mascara de 16 bits
-        out	0A1h,al
-        pop     ebp
-        retn
+	push	ebp
+	mov		ebp, esp
+	mov		ax, [ss:ebp+8]  ; ax = mascara de 16 bits
+	out		0A1h,al
+	pop		ebp
+	retn
 
 _read_msw:
-        smsw    ax		; Obtiene la Machine Status Word
-        retn
+	smsw	ax		; Obtiene la Machine Status Word
+	retn
 
 
 _lidt:				; Carga el IDTR
-        push    ebp
-        mov     ebp, esp
-        push    ebx
-        mov     ebx, [ss: ebp + 6] ; ds:bx = puntero a IDTR 
-	rol	ebx,16		    	
-	lidt    [ds: ebx]          ; carga IDTR
-        pop     ebx
-        pop     ebp
-        retn
+	push	ebp
+	mov		ebp, esp
+	push	ebx
+	mov		ebx, [ss: ebp + 6] ; ds:bx = puntero a IDTR
+	rol		ebx,16
+	lidt	[ds: ebx]          ; carga IDTR
+	pop		ebx
+	pop		ebp
+	retn
 
 
 _int_08_hand:				; Handler de INT 8 ( Timer tick)
-        push    ds
-        push    es                      ; Se salvan los registros
-        pusha                           ; Carga de DS y ES con el valor del selector
-        mov     ax, 10h			; a utilizar.
-        mov     ds, ax
-        mov     es, ax                  
-        call    int_08                 
-        mov	al,20h			; Envio de EOI generico al PIC
-	out	20h,al
-	popa                            
-        pop     es
-        pop     ds
-        iret
+	push	ds
+	push	es                      ; Se salvan los registros
+	pusha                           ; Carga de DS y ES con el valor del selector
+	mov		ax, 10h			; a utilizar.
+	mov		ds, ax
+	mov		es, ax
+	call	int_08
+	mov		al,20h			; Envio de EOI generico al PIC
+	out		20h,al
+	popa
+	pop		es
+	pop		ds
+	iret
 
 _reset:
 .wait1:
-	in   al, 0x64
-	test al, 00000010b
-	jne  .wait1
-	mov  al, 0xFE
-	out  0x64, al
+	in		al, 64h
+	test	al, 02h
+	jne		.wait1
+	mov		al, 0FEh
+	out		64h, al
 	ret
 
-; Debug para el BOCHS, detiene la ejecución; Para continuar colocar en el BOCHSDBG: set $eax=0
-;
+; Debug para el BOCHS, detiene la ejecución
+; Para continuar colocar en el BOCHSDBG: set $eax=0
 
 
 _debug:
-        push    bp
-        mov     bp, sp
-        push	ax
-vuelve:	mov     ax, 1
-        cmp	ax, 0
+	push    bp
+	mov     bp, sp
+	push	ax
+vuelve:
+	mov     ax, 1
+	cmp	ax, 0
 	jne	vuelve
 	pop	ax
 	pop     bp
-        retn
+	retn
