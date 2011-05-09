734878106de9f44b686543eeb13b82c260c7f5eb
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-09 01:07:25 -0300

    removed writing from int08 and implemented int 09

--- src/interrupts.c
+++ src/interrupts.c
@@ -3,17 +3,27 @@
 int tickpos=-2;
 int ticks = 0;
 
+static inline
+unsigned char inb( unsigned short port )
+{
+    unsigned char ret;
+    asm volatile( "inb %1, %0"
+                  : "=a"(ret) : "Nd"(port) );
+    return ret;
+}
+
 //Timer Tick
 void int_08() {
-    char *video = (char *) 0xb8000;
-	ticks++;
-	if(ticks == 18) {
-	    video[(tickpos+=2) % (25 * 80 * 2)]=65 + (tickpos/2) % 26;
-		ticks = 0;
-	}
 }
 
 //Keyboard
+int par = 0;
 void int_09() {
-
+    char *video = (char *) 0xb8000;
+	char i = inb(0x60);
+	par++; //For some reason i'm reading twice
+	if (counter % 2) {
+    	video[(tickpos+=2) % (25 * 80 * 2)]=65 + (tickpos/2) % 26;
+	}
+	return;
 }
