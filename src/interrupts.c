8df287b8677ce9ef1d54c346ce8efc6542199539
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-12 00:54:14 -0300

    moved inb to io.h

--- src/interrupts.c
+++ src/interrupts.c
@@ -1,14 +1,5 @@
 #include "../include/interrupts.h"
 
-static inline
-unsigned char inb( unsigned short port )
-{
-    unsigned char ret;
-    asm volatile( "inb %1, %0"
-                  : "=a"(ret) : "Nd"(port) );
-    return ret;
-}
-
 //Timer Tick
 void int_08() {
 }
