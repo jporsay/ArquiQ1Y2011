b4d5eb47f5073e94ff8d37347f8371eab4c286d7
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-12 00:53:47 -0300

    created io.h

---
+++ include/io.h
@@ -0,0 +1,18 @@
+#ifndef IO_H
+#define IO_H
+static inline
+unsigned char inb( unsigned short port )
+{
+    unsigned char ret;
+    asm volatile( "inb %1, %0"
+                  : "=a"(ret) : "Nd"(port) );
+    return ret;
+}
+
+static inline
+void outb( unsigned short port, unsigned char val )
+{
+    asm volatile( "outb %0, %1"
+                  : : "a"(val), "Nd"(port) );
+}
+#endif
