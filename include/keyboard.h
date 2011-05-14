0743b8dace5f1917f663cc2155214421aa72b302
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-14 17:20:03 -0300

    defines for important keyboard keys. added kbFlags. some copadou macros function definitions.

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,9 +1,23 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-#define KEY_RELEASE 0x80
+#include "kasm.h"
+#include "defs.h"
 
-void handleKeyPress(char scanCode);
-void handleKeyRelease(char scanCode);
+#define IS_MAKE(A) ((A) & 0x80)
+#define CLEAR_BREAK_BIT(A) ((A) & 0x7F)
+#define ESCAPE	0x01
+#define UCASE	0x02
+#define CTRL	0x04
+#define ALT		0x08
+#define DEL		0x10
+#define SHIFT_PRESSED() (kbFlags & UCASE)
 
+char kbFlags;
+
+void handleScanCode(unsigned char scanCode);
+int checkSpecialKey(unsigned char scanCode);
+int specialAscii(char ascii);
+void putc(char ascii);
+char translateSc(unsigned char scanCode);
 #endif
