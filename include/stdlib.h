6d93f3db6d0f3a7f47324254ab58c52debbbf272
Juan Pablo Orsay <jporsay@gmail.com>
2011-05-28 18:47:21 -0300

    added definitions for variable arguments.

--- include/stdlib.h
+++ include/stdlib.h
@@ -3,6 +3,12 @@
 
 #include "defs.h"
 
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_arg(v,l)   __builtin_va_arg(v,l)
+#define va_end(v)     __builtin_va_end(v)
+#define va_copy(d,s)  __builtin_va_copy(d,s)
+typedef __builtin_va_list va_list;
+
 #define STDOUT 1
 
 #define STDIN 2
